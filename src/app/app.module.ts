00200d8cab06aa3c99e272ba02349d7e59912fe6
Taras Hupalo <taras.hupalo@gmail.com>
2017-08-22 16:44:27 +0300

    Create the form radio component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormInputComponent } from './form-input/form-input.component';
+import { FormRadioComponent } from './form-radio/form-radio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientFormComponent,
-    FormInputComponent
+    FormInputComponent,
+    FormRadioComponent
   ],
   imports: [
     BrowserModule,
