f221a9e95eff81e5657be1b969c0cb428249e2bf
Taras Hupalo <taras.hupalo@gmail.com>
2017-08-22 17:46:21 +0300

    Use the @Host() decorator in the dynamic field directive

--- src/app/dynamic-field.directive.ts
+++ src/app/dynamic-field.directive.ts
@@ -1,5 +1,5 @@
-import { ComponentFactoryResolver, Directive, Input, OnInit, ViewContainerRef } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { ComponentFactoryResolver, Directive, Host, Input, OnInit, ViewContainerRef } from '@angular/core';
+import { FormGroupDirective } from '@angular/forms';
 import { FormInputComponent } from './form-input/form-input.component';
 import { FormRadioComponent } from './form-radio/form-radio.component';
 
@@ -13,15 +13,16 @@ const types = {
 })
 export class DynamicFieldDirective implements OnInit {
 
-  @Input() group: FormGroup;
   @Input() config: any;
 
-  constructor(private container: ViewContainerRef, private resolver: ComponentFactoryResolver) { }
+  constructor(private container: ViewContainerRef,
+              private resolver: ComponentFactoryResolver,
+              @Host() private groupDirective: FormGroupDirective) { }
 
   ngOnInit() {
     const componentFactory = this.resolver.resolveComponentFactory<any>(types[this.config.type]);
     const componentRef = this.container.createComponent(componentFactory);
-    componentRef.instance.group = this.group;
+    componentRef.instance.group = this.groupDirective.form;
     componentRef.instance.config = this.config;
   }
 }
